f6af51f895771de9382662c6c1e0f80c3c3c70be
Tesla <67636538+tesla1618@users.noreply.github.com>
2022-10-02 16:00:06 +0600

    Add files via upload

---
+++ header.py
@@ -0,0 +1,7 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+import sys, os
+from PyQt5 import uic, QtGui
+
+thisPath = os.path.dirname(os.path.abspath(__file__))
+uiPath = os.path.join(thisPath, 'ui')
